b65091d7a9d973395569b811ffa9f57e6149d0e6
key262yek <key262yek@gmail.com>
2021-12-29 11:46:50 +0900

    Update hamiltonian iterator
    Coefficient 자유도를 높임

--- src/hamiltonian/mod.rs
+++ src/hamiltonian/mod.rs
@@ -2,15 +2,45 @@
 use genawaiter::{sync::gen, sync_producer, yield_};
 use crate::prelude::*;
 
-
-pub struct PeriodicNearestXXZ{
-    pub delta : f64
+#[derive(Copy, Clone, Debug)]
+pub struct PeriodicIsing{
+    pub delta : f64,
 }
 
-impl PeriodicNearestXXZ{
+impl PeriodicIsing{
     pub fn new(delta : f64) -> Self{
         Self{
             delta,
+        }
+    }
+
+    pub fn apply_to<S>(&self, state : &S) -> f64
+        where S : State{
+
+        let mut sum = 0f64;
+        for (si, sj) in state.periodic_pair_iter(){
+            if si == sj{
+                sum -= self.delta / 2f64;
+            } else {
+                sum += self.delta / 2f64;
+            }
+        }
+
+        return sum;
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct PeriodicNearestXXZ{
+    pub delta_x : f64,
+    pub delta_z : f64,
+}
+
+impl PeriodicNearestXXZ{
+    pub fn new(delta_x : f64, delta_z : f64) -> Self{
+        Self{
+            delta_x,
+            delta_z,
         }
     }
 
@@ -21,11 +51,11 @@ impl PeriodicNearestXXZ{
             let mut sum = 0f64;
             for ((i, si), (j, sj)) in state.periodic_pair_enumerate(){
                 if si == sj{
-                    sum -= self.delta / 2f64;
+                    sum -= self.delta_z / 2f64;
                 } else {
-                    sum += self.delta / 2f64;
+                    sum += self.delta_z / 2f64;
                     let flipped = bit_flip_unsafe(num, i, j);
-                    yield_!((flipped, -1f64));
+                    yield_!((flipped, -self.delta_x));
                 }
             }
 
@@ -34,19 +64,20 @@ impl PeriodicNearestXXZ{
     }
 }
 
+#[derive(Copy, Clone, Debug)]
 pub struct PeriodicNextNearestXXZ{
-    pub delta : f64
+    pub delta_x1 : f64,
+    pub delta_x2 : f64,
+    pub delta_z : f64,
 }
 
 impl PeriodicNextNearestXXZ{
-    pub fn new(delta : f64) -> Self{
+    pub fn new(delta_x1 : f64, delta_x2 : f64, delta_z : f64) -> Self{
         Self{
-            delta,
+            delta_x1,
+            delta_x2,
+            delta_z,
         }
-    }
-
-    pub fn delta(&self) -> f64{
-        self.delta
     }
 
     pub fn apply_to<'a, S>(&'a self, state : &'a S) -> impl Iterator<Item = (usize, f64)> + 'a
@@ -56,18 +87,18 @@ impl PeriodicNextNearestXXZ{
             let mut sum = 0f64;
             for ((i, si), (j, sj)) in state.periodic_pair_enumerate(){
                 if si == sj{
-                    sum -= self.delta / 2f64;
+                    sum -= self.delta_z / 2f64;
                 } else {
-                    sum += self.delta / 2f64;
+                    sum += self.delta_z / 2f64;
                     let flipped = bit_flip_unsafe(num, i, j);
-                    yield_!((flipped, -1f64));
+                    yield_!((flipped, -self.delta_x1));
                 }
             }
 
             for ((i, si), (j, sj)) in state.periodic_distanced_pair_enumerate(2){
                 if si != sj{
                     let flipped = bit_flip_unsafe(num, i, j);
-                    yield_!((flipped, -1f64));
+                    yield_!((flipped, -self.delta_x2));
                 }
             }
 
@@ -88,7 +119,7 @@ mod test {
     fn test_apply_hamiltonian(){
 
         let delta = 2f64;
-        let x: PeriodicNearestXXZ  = PeriodicNearestXXZ::new(delta);
+        let x: PeriodicNearestXXZ  = PeriodicNearestXXZ::new(1f64, delta);
         for data in x.apply_to(&SimpleState::new(2, 4)){
             assert!((data == (1, -1f64)) || (data == (4, -1f64)) || (data == (2, 0f64)));
         }
